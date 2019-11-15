cd16492e6cf004d801d9dc1ba451568a0d7b189a
Milton Ocampo <47097454+miltonf55@users.noreply.github.com>
2019-11-14 20:55:17 -0600

    Version incio facebook

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mysql=require('mysql');
 const session = require('express-session');
 const path = require('path');
 const CryptoJS = require("crypto-js");
+const passport = require('passport'), FacebookStrategy = require('passport-facebook').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 var app=express();
 var bodyParser=require('body-parser');
@@ -14,6 +16,31 @@ var con= mysql.createConnection({
 });
 
 con.connect();
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate('', function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
 
 app.set('view engine', 'jade');
 app.use( bodyParser.json() )
